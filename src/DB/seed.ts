1715eb68e83e5ffa857461e1803995a304f4cee8
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-06-01 07:43:45 +0600

    sper email

--- src/DB/seed.ts
+++ src/DB/seed.ts
@@ -1,35 +1,35 @@
-import bcrypt from "bcrypt";
-import {
-  Gender,
-  PrismaClient,
-  UserRole,
-  UserStatus,
-} from "../../generated/prisma";
-import config from "../config";
+// import bcrypt from "bcrypt";
+// import {
+//   Gender,
+//   PrismaClient,
+//   UserRole,
+//   UserStatus,
+// } from "../../generated/prisma";
+// import config from "../config";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const ensureSuperAdmin = async () => {
-  const existing = await prisma.user.findFirst({
-    where: { role: UserRole.SUPER_ADMIN },
-  });
+// const ensureSuperAdmin = async () => {
+//   const existing = await prisma.user.findFirst({
+//     where: { role: UserRole.SUPER_ADMIN },
+//   });
 
-  if (!existing) {
-    await prisma.user.create({
-      data: {
-        name: "Super Admin",
-        email: `${config.super_admin}`,
-        password: await bcrypt.hash(`${config.super_pass}`, 10),
-        role: UserRole.SUPER_ADMIN,
-        profilePhoto:"https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg",
-        gender: Gender.MALE,
-        status: "ACTIVE",
-        needPasswordChange: false,
-      },
-    });
-  } else {
-    console.log("✅ Super Admin already exists");
-  }
-};
+//   if (!existing) {
+//     await prisma.user.create({
+//       data: {
+//         name: "Super Admin",
+//         email: `${config.super_admin}`,
+//         password: await bcrypt.hash(`${config.super_pass}`, 10),
+//         role: UserRole.SUPER_ADMIN,
+//         profilePhoto:"https://img.freepik.com/premium-vector/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-vector-illustration_561158-4195.jpg",
+//         gender: Gender.MALE,
+//         status: "ACTIVE",
+//         needPasswordChange: false,
+//       },
+//     });
+//   } else {
+//     console.log("✅ Super Admin already exists");
+//   }
+// };
 
-export default ensureSuperAdmin;
+// export default ensureSuperAdmin;
